e3c2681d09047ff2cdcb87c0f6b34f4eb9a333a4
Dmitry Rasskazov <rasskazov@bringo.com>
2024-09-24 18:14:07 +0500

    fix-ternary-operator

--- laba4/src/main/java/tech/rasskazov/laba/informationalSystemDesign/laba4/examples/TernaryOperator.java
+++ laba4/src/main/java/tech/rasskazov/laba/informationalSystemDesign/laba4/examples/TernaryOperator.java
@@ -2,14 +2,28 @@ package tech.rasskazov.laba.informationalSystemDesign.laba4.examples;
 
 public class TernaryOperator implements Runnable
 {
-    private static boolean flag1 = false;
-    private static boolean flag2 = true;
+    private static boolean flag = false;
+    private static boolean flag1 = true;
+    private static boolean flag2 = false;
 
     @Override
     public void run()
     {
-        Integer n = flag1 ? 1 : flag2 ? 2 : null;
+        this.example1();
+        this.example2();
         System.out.println(subExample(5));
+    }
+
+    private void example1()
+    {
+        Number n = flag ? new Integer(1) : new Double(2.0);
+        System.out.println(n);
+    }
+
+    private void example2()
+    {
+        Integer n = flag1 ? 1 : flag2 ? 2 : null;
+        System.out.println(n);
     }
 
     private double subExample(int idx)
